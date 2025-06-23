bb141079025d01a484914482992483ef227214c2
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-06-23 22:32:05 +0300

    initial

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,16 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+
+export const PostInfo = ({ post }) => (
+  <div className="PostInfo">
+    <div className="PostInfo_header">
+      <h3 className="PostInfo_title">{post.title}</h3>
+      <UserInfo user={post.user} />
+    </div>
+    <p className="PostInfo_body">{post.body}</p>
+    <hr />
+    <CommentList comments={post.comments} />
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </div>
+);
